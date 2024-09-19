da0488064761e1aebf23163d4d7ac73485964579
Meg <marguerite832@gmail.com>
2024-09-20 06:41:42 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './Button'
+
+
+const App=() => {
+    return(
+        <div>
+            <div>App</div>
+            <Button title="Post"/>
+            <Button title="Edit"/>
+            <Button title="Delete"/>
+            <Button title="Submit"/>
+        </div>
+    )
+}
+
+export default App;
